58953a65bf21a374f9fb939a29ee23c02ba1c98e
UvA Programming Lab <terra@proglab.nl>
2025-02-04 13:23:58 +0100

    Update hello.c

--- hello.c
+++ hello.c
@@ -6,8 +6,13 @@ int main(void)
     printf("what is your name?\n");
 }
 
-void main(void)
-
+int main (void)
 {
-    
+  char name;
+
+ printf("Let's do this, please enter your name:");
+ scanf("%s", &name);
+ printf("Your name is %s", name);
+
+ return 0;
 }
