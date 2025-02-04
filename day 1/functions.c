01dd71b65f3d80b763081d07c411b50ab01dd335
UvA Programming Lab <terra@proglab.nl>
2025-02-04 13:32:19 +0100

    Update day 1/functions.c

---
+++ day 1/functions.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int times_two(int a);
+void print_int(int a);
+
+int main(void)
+{
+    int x = 2;
+    int y = times_two(x);
+
+    print_int(y);
+}
+
+int times_two(int a)
+{
+    return a * 2;
+}
+
+void print_int(int a)
+{
+    printf("Value = %i\n", a);
+}
