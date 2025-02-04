109705025a40f78330b4bb185770bf03d562275c
secondlocket <benjaminchan031@gmail.com>
2025-02-04 13:38:48 +0100

    Update functions.c

--- day 1/functions.c
+++ day 1/functions.c
@@ -1,28 +1,22 @@
 #include <stdio.h>
 
-int times_two(int a);
+int half(int a);
 void print_int(int a);
 
 int main(void)
 {
     int x = 2;
-    int y = times_two(x);
+    int y = half(x);
 
     print_int(y);
 }
 
-int times_two(int a)
+int half(int a)
 {
-    return a * 2;
+    return a / 2;
 }
 
 void print_int(int a)
 {
     printf("Value = %i\n", a);
-}
-
-
-int half(int a)
-{
-    return a/2;
 }
