00235fc6d44943b58018fe7f88b55a4da9d6ab56
UvA Programming Lab <terra@proglab.nl>
2025-02-06 15:07:14 +0100

    Update prog1/day 3/coupons.c

--- prog1/day 3/coupons.c
+++ prog1/day 3/coupons.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <cs50.h>
-//ik heb "optie1, optie2, optie3" gebruikt in plaats van bereken_coupon1bereken_coupon1bereken_coupon1
+//ik heb "optie1, optie2, optie3" gebruikt in plaats van bereken_coupon1
+//bereken_coupon1bereken_coupon1
 int main () {
     int stuks = get_int("Hoeveel stuks koop je? = ");
     float prijs = get_float("Wat is de prijs per stuk? = ");
