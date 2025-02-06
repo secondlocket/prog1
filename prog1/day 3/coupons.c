dd4b4966f89b9c5a9f0abb3d9204d85dc33c7c36
UvA Programming Lab <terra@proglab.nl>
2025-02-06 14:37:49 +0100

    Update prog1/day 3/coupons.c

--- prog1/day 3/coupons.c
+++ prog1/day 3/coupons.c
@@ -5,7 +5,11 @@ int main () {
     int stuks = get_int("Hoeveel stuks koop je? = ");
     float prijs = get_float("Wat is de prijs per stuk? = ");
     
-    float optie1 = 
+    float optie1 = (stuks - (stuks/3)) * prijs  
+    float optie2 = 
+    float optie3 = 
+    
+    
     if (stuks/3 = int)
     {
         (stuks - (stuks/3)) * prijs 
