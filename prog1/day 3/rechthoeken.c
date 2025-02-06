75d240cc243c0c418419244d6a00e150dbe1f871
UvA Programming Lab <terra@proglab.nl>
2025-02-06 14:17:38 +0100

    Update prog1/day 3/rechthoeken.c

--- prog1/day 3/rechthoeken.c
+++ prog1/day 3/rechthoeken.c
@@ -22,6 +22,7 @@ int main() {
         case '2':
         printf("Oppervlakte van de tweede rechthoek: %d\n", oppervlakte(lengte2, breedte2));
                 break;
+        case 'S'
         case ''
     }
     
