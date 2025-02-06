04441909a30db7b38b5377138cf5eda635e1d4b3
UvA Programming Lab <terra@proglab.nl>
2025-02-06 14:52:47 +0100

    Update prog1/day 3/coupons.c

--- prog1/day 3/coupons.c
+++ prog1/day 3/coupons.c
@@ -17,7 +17,7 @@ float optie3(int stuks, float prijs) {
     return stuks * prijs * 0.9
     
     else if (int = 3)
-    return
+    return stuks * prijs * 
     
     else 
     return stuks * prijs * 0.7
