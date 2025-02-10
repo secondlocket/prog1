a4b3c58538805eef1ef58a5a4772af09db55ad58
UvA Programming Lab <terra@proglab.nl>
2025-02-10 20:49:02 +0100

    Update prog1/week 1/day 1/binary.c

--- prog1/week 1/day 1/binary.c
+++ prog1/week 1/day 1/binary.c
@@ -3,5 +3,19 @@
 
 int main ()
 {
-    
+    #include <stdio.h>
+
+// Functie om individuele bits om te zetten naar een decimaal getal
+int to_decimal(int bit1, int bit2, int bit3, int bit4) {
+    return (bit1 << 3) | (bit2 << 2) | (bit3 << 1) | bit4;
+}
+
+int main() {
+    // Testcases
+    printf("%d\n", to_decimal(0, 0, 0, 1)); // Verwacht: 1
+    printf("%d\n", to_decimal(0, 1, 0, 1)); // Verwacht: 5
+    printf("%d\n", to_decimal(1, 1, 1, 1)); // Verwacht: 15
+    return 0;
+}
+
 }
