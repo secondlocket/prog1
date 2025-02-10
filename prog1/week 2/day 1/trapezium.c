daafef2cd1ffae0e3200aa28b22bd5e293c83079
UvA Programming Lab <terra@proglab.nl>
2025-02-10 14:42:38 +0100

    Update prog1/week 2/day 1/trapezium.c

--- prog1/week 2/day 1/trapezium.c
+++ prog1/week 2/day 1/trapezium.c
@@ -36,7 +36,7 @@ int main (int argc, string argv[])
     //trapezium zelf
     for (i = 0; i < n; i++)
     {
-        printf("#");
+        
         for (j = 0; j < n * 2 - 2; j++)
         {
             printf(" ");
