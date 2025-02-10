1cdd6ac845a70184360f6950168577c604bc71ed
UvA Programming Lab <terra@proglab.nl>
2025-02-10 22:13:28 +0100

    Update prog1/week 1/day 1/binary.c

--- prog1/week 1/day 1/binary.c
+++ prog1/week 1/day 1/binary.c
@@ -8,8 +8,8 @@ int to_decimal(int bit1, int bit2, int bit3, int bit4, int bit5)
 
 int main() 
 {
-    printf("%d\n", to_decimal(0, 0, 0, 1));
-    printf("%d\n", to_decimal(0, 1, 0, 1));
+    printf("%d\n", to_decimal(0, 0, 0, 0, 1));
+    printf("%d\n", to_decimal(0, 0, 1, 0, 1));
     printf("%d\n", to_decimal(1, 1, 1, 1, 1));
 
     return 0;
