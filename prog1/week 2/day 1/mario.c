e92bb51ca31fc6dfd546d749147bf76fd1876c8e
UvA Programming Lab <terra@proglab.nl>
2025-02-10 13:20:30 +0100

    Update prog1/week 2/day 1/mario.c

--- prog1/week 2/day 1/mario.c
+++ prog1/week 2/day 1/mario.c
@@ -5,6 +5,6 @@ int main () {
     int n;
     do
     {
-        = get_int("hoe hoog is je pyramide? (typ iets tussen 1 en 8 in) = ");
-    
+        n = get_int("hoe hoog is je pyramide? (typ iets tussen 1 en 8 in) = ");
+    }
 }
