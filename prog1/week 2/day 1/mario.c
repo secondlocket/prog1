12c14324daa21e6818c7732643e6666d30b2bd74
UvA Programming Lab <terra@proglab.nl>
2025-02-10 13:20:03 +0100

    Update prog1/week 2/day 1/mario.c

--- prog1/week 2/day 1/mario.c
+++ prog1/week 2/day 1/mario.c
@@ -2,5 +2,6 @@
 #include <cs50.h>
 
 int main () {
-    int n = get_int("hoe hoog is je pyramide? (typ iets tussen 1 en 8 in) = \n")
+    int n = get_int("hoe hoog is je pyramide? (typ iets tussen 1 en 8 in) = ");
+    
 }
