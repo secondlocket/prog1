89ce7df97bd25e369d475f6c3f2c42de9417c2cd
UvA Programming Lab <terra@proglab.nl>
2025-02-10 14:30:23 +0100

    Update prog1/week 2/day 1/trapezium.c

--- prog1/week 2/day 1/trapezium.c
+++ prog1/week 2/day 1/trapezium.c
@@ -17,7 +17,7 @@ int main ()
         //eerste regel
     if(j > 0)
         {
-            for (i = 0; i < n - 1; i++)
+            for (j = 0; j < n - 1; j++)
             {
                 printf(" ");
             }
