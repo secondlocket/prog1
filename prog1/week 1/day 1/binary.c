0e1de26bd5e321717530fd65ca4b25f414209d7a
UvA Programming Lab <terra@proglab.nl>
2025-02-10 22:12:18 +0100

    Update prog1/week 1/day 1/binary.c

--- prog1/week 1/day 1/binary.c
+++ prog1/week 1/day 1/binary.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <cs50.h>
 
-int to_decimal(int bit1, int bit2, int bit3, int bit4) 
+int to_decimal(int bit1, int bit2, int bit3, int bit4, int bit5) 
 {
     return (bit1 << 3) | (bit2 << 2) | (bit3 << 1) | bit4;
 }
