10c1704196bd5e5a58dc84050636629868c34bd4
UvA Programming Lab <terra@proglab.nl>
2025-02-10 12:02:52 +0100

    Rename mario.c to prog1/week 2/day 1/mario.c

---
+++ prog1/week 2/day 1/mario.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <cs50.h>
+
+int main ()
+{
+    int n;
+    do
+    {
+        n = get_int("hoe hoog is je piramide? (typ iets tussen 1 en 8 in) = ");
+        
+    }
+    while(n<1 || n>8);
+    
+    for(int i = 0; i < n; i++)
+    {
+        printf("#\n");
+    }
+    
+    return 0;
+}
