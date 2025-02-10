4bbbf75f23bb8ffdb0e2eff3d47df87e3148cc25
UvA Programming Lab <terra@proglab.nl>
2025-02-10 13:58:37 +0100

    Update driehoek.c

--- driehoek.c
+++ driehoek.c
@@ -21,7 +21,7 @@ int main ()
         printf("#");
         
         //spatie binnen
-        for(int j = 1; j <= i; j++)
+        for(int j = 0; j <= i; j+2)
         {
             printf(".");
         }
