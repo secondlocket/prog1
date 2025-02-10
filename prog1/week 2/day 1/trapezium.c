f7d5a3cb0b061edb552af02cae0834163973b705
UvA Programming Lab <terra@proglab.nl>
2025-02-10 14:21:04 +0100

    Update prog1/week 2/day 1/trapezium.c

--- prog1/week 2/day 1/trapezium.c
+++ prog1/week 2/day 1/trapezium.c
@@ -11,7 +11,13 @@ int main ()
     while(n < 5 || n > 20)
     
     //eerste regel
-    
+    for(i = 0; i < n - 1; i++)
+    {
+        //spatie links
+        for (int y = 0; y < n - i - 1; y++)
+        {
+            printf(" ");
+        }
     
     return 0;
 }
