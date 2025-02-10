d6365b139da6c9ec50da3e9d1b4ba95eddaecb12
UvA Programming Lab <terra@proglab.nl>
2025-02-10 14:09:52 +0100

    Update driehoek.c

--- driehoek.c
+++ driehoek.c
@@ -11,7 +11,7 @@ int main ()
     }
     while(n < 5 || n > 20);
     
-    for(i = 0; i < n; i++)
+    for(i = 0; i < n - 1; i++)
     {
         //spatie buiten
         for (int y = 0; y < n - i - 1; y++)
