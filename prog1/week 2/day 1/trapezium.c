5ef13d29a59e754fa6eb67c9be8e00a5ffa28857
UvA Programming Lab <terra@proglab.nl>
2025-02-10 15:26:05 +0100

    Update prog1/week 2/day 1/trapezium.c

--- prog1/week 2/day 1/trapezium.c
+++ prog1/week 2/day 1/trapezium.c
@@ -56,7 +56,7 @@ int main (int argc, string argv[])
         printf("#");
         
         //spaties R
-            for (j = 0; y < n - i - 1; y++)
+            for (j = 0; y < n - 1; y++)
             {
                 printf(".");
             }
