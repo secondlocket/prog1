73e491197101da1ec3bea5a035b4d62d0df55a5b
UvA Programming Lab <terra@proglab.nl>
2025-02-10 11:10:26 +0100

    Update prog1/week 2/day 1/repeat.c

--- prog1/week 2/day 1/repeat.c
+++ prog1/week 2/day 1/repeat.c
@@ -5,7 +5,7 @@
 int main () 
 {
     string input_w = get_string("What do you want to say? = ");
-    int input_n = get_int("Hoeveel keren wil je dit zeggen? = ");
+    int input_n = get_int("How many times do you want to say this? = ");
     
     int i = 0;
     
