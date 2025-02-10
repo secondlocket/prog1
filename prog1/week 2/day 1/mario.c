522cea3791042975a72f2dac4f93fd9484fd7ac3
UvA Programming Lab <terra@proglab.nl>
2025-02-10 13:02:19 +0100

    Update prog1/week 2/day 1/mario.c

--- prog1/week 2/day 1/mario.c
+++ prog1/week 2/day 1/mario.c
@@ -31,11 +31,11 @@ int main ()
 
 
         for (int i = 0; i < n; i++)
-  {
-      for (int j = 0; j < n; j++)
-      {
+        {
+        for (int j = 0; j < n; j++)
+        {
           printf("#");
-      }
+        }
       printf("\n");
   }
     }
