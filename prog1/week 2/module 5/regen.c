d6b1924f31aecf70ddd787c9e595eced3f2050bf
UvA Programming Lab <terra@proglab.nl>
2025-02-11 13:10:34 +0100

    Rename module 5/regen.c to prog1/week 2/module 5/regen.c

---
+++ prog1/week 2/module 5/regen.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <cs50.h>
+
+int main () 
+{
+    int h = 0;
+    int aantal_dagen = 0;
+    float totaal_regen = 0;
+
+    printf("Voer hoeveelheid van regen per dag, typ 999 in om af te sluiten\n");
+
+    do
+    {
+        h = get_int("Hoeveel: ");
+    
+        aantal_dagen++;
+        totaal_regen = totaal_regen + h;
+        
+        if (h == 999)
+        {
+            printf("Dat kan niet");
+            break;
+        }
+        
+        if (h==0)
+        {
+            float g = totaal_regen / (aantal_dagen - 1);
+            printf("Gemiddeld %0.3f millimeter", g);
+            break;
+        }
+    }
+    while (h < 999);
+  
+    return 0;
+}
