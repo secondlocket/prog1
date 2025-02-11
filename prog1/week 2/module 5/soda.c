51212a95c3ad9e568a5266c7aba35fddaf2ec36d
UvA Programming Lab <terra@proglab.nl>
2025-02-11 23:47:14 +0100

    Rename soda.c to prog1/week 2/module 5/soda.c

---
+++ prog1/week 2/module 5/soda.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <cs50.h>
+
+int main ()
+{
+    //te betalen bedrag
+    int t = 50;
+    //n = geld ingevoerd 
+    int ins = 0;
+    
+    printf("50 cents owed\n");
+    
+    do 
+    {
+        //money inserted
+        int n = get_int ("Insert coin = ");
+        
+        if (n != 5 && n != 10 && n != 25)
+        {
+            printf("Sorry, we only accept 5, 10 or 25 cents, please try again\n");
+            continue;
+        }
+        
+        ins += n;
+        
+        if (ins < t)
+        {
+            printf("%.2d cents owed\n", (t - ins));
+        }
+        else
+        {
+            printf("%d cents change\n", ins - t);
+            break;
+        }
+    }
+    while (ins < t);
+
+    return 0;
+}
