e24e5a8a884e1167fc91c4782faba925f5cfe480
UvA Programming Lab <terra@proglab.nl>
2025-02-11 12:27:48 +0100

    Update regen.c

--- regen.c
+++ regen.c
@@ -7,15 +7,17 @@ int main ()
     
     printf("Voer hoeveelheid van regen per dag, typ 999 in om af te sluiten\n");
     
-    if (h == 999)
-        {
-            printf("Dat kan niet");
-        }
+    
     
     do
     {
         h = get_int("Hoeveel: ");
         
+        if (h == 999)
+        {
+            printf("Dat kan niet");
+            break;
+        }
     }
     while (h < 999);
    
