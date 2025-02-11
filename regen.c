03f4a4e6259eb361bc7205987da47774f27932af
UvA Programming Lab <terra@proglab.nl>
2025-02-11 12:51:36 +0100

    Update regen.c

--- regen.c
+++ regen.c
@@ -25,7 +25,7 @@ int main ()
     }
     while (h < 999);
    
-   int g = totaal_regen / aantal_dagen
+   int g = totaal_regen / aantal_dagen;
    
     printf("Gemiddeld %.3d millimeter", g);
     
