d6d54dceee00a3dc0d55938164eea975acca5b20
UvA Programming Lab <terra@proglab.nl>
2025-02-13 15:35:57 +0100

    Update prog1/week 2/module 6/morse.c

--- prog1/week 2/module 6/morse.c
+++ prog1/week 2/module 6/morse.c
@@ -46,8 +46,8 @@ int main()
     
     for (int i = 0; i < check; i += 3)
     {
-        char morse_array[4] = {morse[i], morse[i+1], morse[i+2], '\0'}
-        char letter = omzetting(morse_array)
+        char morse_array[4] = {morse[i], morse[i+1], morse[i+2], '\0'};
+        char letter = omzetting(morse_array);
         printf("%c", letter);
     }
     
