064b0270a0760b7e40fdeefb576b0558150f92d9
UvA Programming Lab <terra@proglab.nl>
2025-02-13 15:39:34 +0100

    Update prog1/week 2/module 6/morse.c

--- prog1/week 2/module 6/morse.c
+++ prog1/week 2/module 6/morse.c
@@ -38,7 +38,7 @@ int main()
             
         //controleren dat het deelbaar door 3 is
         int check = strlen(morse);
-            if (check % 3 != 0)
+        if (check % 3 != 0)
         {
             printf("Ongeldige morse code\n");
         }
