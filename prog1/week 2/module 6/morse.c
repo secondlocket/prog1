b46cbf4df4080f22fa084ac39e3b83008c84671d
UvA Programming Lab <terra@proglab.nl>
2025-02-13 15:28:19 +0100

    Update prog1/week 2/module 6/morse.c

--- prog1/week 2/module 6/morse.c
+++ prog1/week 2/module 6/morse.c
@@ -22,7 +22,7 @@ char omzetting(const char* morse)
     {
         return("D");
     }
-    else if (strcmp(morse, '.-.') == 0)
+    else if (strcmp(morse, ".-.") == 0)
     {
         return("R");
     }
