654d674c89851750840c0bc6a04118de9540b52c
UvA Programming Lab <terra@proglab.nl>
2025-02-13 14:00:35 +0100

    Update prog1/week 2/module 6/tennis.c

--- prog1/week 2/module 6/tennis.c
+++ prog1/week 2/module 6/tennis.c
@@ -16,12 +16,12 @@ int main ()
     
     if (beurt == 1)
     {
-    do
-    {
-        string speler1 = get_string("Speler 1, geef een woord = ");
-        string speler2 = get_string("Speler 2, geef een woord = ");
-    }
-    while()
+        do
+        {
+            string speler1 = get_string("Speler 1, geef een woord = ");
+            string speler2 = get_string("Speler 2, geef een woord = ");
+        }
+        while()
     
     return 0;
 }
