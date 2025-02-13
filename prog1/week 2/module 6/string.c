3ed6e65be5c05190db7957b354402120b6715f84
UvA Programming Lab <terra@proglab.nl>
2025-02-13 12:01:56 +0100

    Update prog1/week 2/module 6/string.c

--- prog1/week 2/module 6/string.c
+++ prog1/week 2/module 6/string.c
@@ -10,7 +10,7 @@ int input1
         int n = strlen(input1);
         do
         {
-            printf(string[0]\n);
+            printf("string[0]\n");
             n--;
         }
         while (n > 0);
