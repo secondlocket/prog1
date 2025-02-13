aee550a82be5b4be5bbb45ec0f7cb47f64771ed8
UvA Programming Lab <terra@proglab.nl>
2025-02-13 13:40:47 +0100

    Update prog1/week 2/module 6/tennis.c

--- prog1/week 2/module 6/tennis.c
+++ prog1/week 2/module 6/tennis.c
@@ -5,7 +5,8 @@
 int main ()
 {
     do
-    string speler1 = get_string("Speler 1, geef een woord = ");
+    {
+        string speler1 = get_string("Speler 1, geef een woord = ");
     string speler2 = get_string("Speler 2, geef een woord = ");
     
     return 0;
