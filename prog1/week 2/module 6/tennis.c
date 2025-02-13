c44a63ddbdebe30a34457f81ebba9691b2dd168c
UvA Programming Lab <terra@proglab.nl>
2025-02-13 14:24:54 +0100

    Update prog1/week 2/module 6/tennis.c

--- prog1/week 2/module 6/tennis.c
+++ prog1/week 2/module 6/tennis.c
@@ -17,7 +17,8 @@ int main ()
     //speler1 
     if (beurt == 1)
     {
-        if (laaste_letter != )
+        if (laaste_letter != 0)
+        {
         string speler1 = get_string("Speler 1, geef een woord = ");
     }
     
