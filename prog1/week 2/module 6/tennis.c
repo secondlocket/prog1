7bf39a0b8532258ab37428e3f594f6bea6147bec
UvA Programming Lab <terra@proglab.nl>
2025-02-13 14:40:21 +0100

    Update prog1/week 2/module 6/tennis.c

--- prog1/week 2/module 6/tennis.c
+++ prog1/week 2/module 6/tennis.c
@@ -20,35 +20,7 @@ int main ()
     {
         
     }
-    //speler1 
-    if (beurt == 1)
-    {
-        string speler1 = get_string("Speler 1, geef een woord = ");
-        if (laaste_letter == 0 || begin(speler1, laaste_letter))
-        {
-            laaste_letter = speler1[strlen(speler1) - 1];
-            beurt = 2;
-        }
-        else
-        {
-            printf("Speler 2 wint!");
-        }
-    }
     
-    //speler2 
-    if (beurt == 2)
-    {
-        string speler2 = get_string("Speler 2, geef een woord = ");
-        if (begin(speler2, laaste_letter))
-        {
-            laaste_letter = speler2[strlen(speler2) - 1];
-            beurt = 2;
-        }
-        else
-        {
-            printf("Speler 1 wint!");
-            break;
-        }
     }
     
     return 0;
