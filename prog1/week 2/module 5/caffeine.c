373625a83ad4e3bb02458422f80f37b401b29d63
UvA Programming Lab <terra@proglab.nl>
2025-02-16 23:52:22 +0100

    Rename caffeine.c to prog1/week 2/module 5/caffeine.c

---
+++ prog1/week 2/module 5/caffeine.c
@@ -0,0 +1,87 @@
+#include <stdio.h>
+#include <cs50.h>
+#include <math.h>
+
+int main ()
+{
+    float g = 0;
+    int mg = 0;
+    int drink = 0;
+    //espresso 70mg
+    int e = 0;
+    //black_tea 30mg
+    int b = 0;
+    //green tea 20mg
+    int gt = 0;
+    //cocoa 10mg
+    int c = 0;
+    //piece of chocolate 1mg
+    int pc = 0;
+    
+    do
+    {
+        g = get_float("Amount in grams = ");
+    }
+    while(g < 0.001);
+    
+    mg = (int) round(g * 1000);
+    
+    do
+    {
+        if (mg >= 70)
+        {
+            mg -= 70;
+            drink++;
+            e++;
+        }
+        else if (mg >= 30)
+        {
+            mg -= 30;
+            drink++;
+            b++;
+        }
+        else if (mg >= 20)
+        {
+            mg -= 20;
+            drink++;
+            gt++;
+        }
+        else if (mg >= 10)
+        {
+            mg -= 10;
+            drink++;
+            c++;
+        }
+        else if (mg >= 1)
+        {
+            mg -= 1;
+            pc++;
+        }
+    }
+    while (mg > 0);
+    
+    if (e > 0)
+    {
+        printf("%i espresso%s\n", e, e == 1 ? "" : "s");
+    }
+    if (b > 0)
+    {
+        printf("%i black tea%s\n", b, b == 1 ? "" : "s");
+    }
+    if (gt > 0)
+    {
+        printf("%i green tea%s\n", gt, gt == 1 ? "" : "s");
+    }
+    if (c > 0)
+    {
+        printf("%i cocoa%s\n", c, c == 1 ? "" : "s");
+    }
+    if (pc > 0)
+    {
+        printf("%i piece of chocolate%s\n", pc, pc == 1 ? "" : "s");
+    }
+    
+    printf("That makes %i drink%s in total\n", drink, drink == 1 ? "" : "s");
+    
+    return 0;
+}
