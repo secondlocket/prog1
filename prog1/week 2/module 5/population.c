a1a694dd9a285603408c9232471c75220813ccf5
UvA Programming Lab <terra@proglab.nl>
2025-02-16 22:29:29 +0100

    Update prog1/week 2/module 5/population.c

--- prog1/week 2/module 5/population.c
+++ prog1/week 2/module 5/population.c
@@ -13,13 +13,13 @@ int main (void)
     while (n < 9);
     
     // TODO: prompt for end size
-int n;
+    int x;
     
     do
     {
-        n = get_int("Start size: ");
+        x = get_int("Start size: ");
     }
-    while (n < 9);
+    while (x < 9);
     // TODO: calculate number of years until we reach threshold
 
     // TODO: print number of years
