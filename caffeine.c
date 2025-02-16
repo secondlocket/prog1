df967511591524e5f1f663c96735a4698a007e6d
UvA Programming Lab <terra@proglab.nl>
2025-02-16 23:51:23 +0100

    Update caffeine.c

--- caffeine.c
+++ caffeine.c
@@ -67,7 +67,7 @@ int main ()
     }
     if (b > 0)
     {
-        printf("%i black tea\n", b, b == 1 ? "" : "s");
+        printf("%i black tea%s\n", b, b == 1 ? "" : "s");
     }
     if (gt > 0)
     {
