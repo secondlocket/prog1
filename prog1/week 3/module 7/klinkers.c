d15d5b10d6a9c6c04ec1bc67119be113b3ea914e
UvA Programming Lab <terra@proglab.nl>
2025-02-17 15:51:58 +0100

    Update prog1/week 3/module 7/klinkers.c

--- prog1/week 3/module 7/klinkers.c
+++ prog1/week 3/module 7/klinkers.c
@@ -10,6 +10,7 @@ int main ()
     string woord1 = get_string("Woord 1 = ");
     string woord2 = get_string("Woord 2 = ");
     
+    lower1
     
     int klinkers1 = klinkers(woord1);
     int klinkers2 = klinkers(woord2);
