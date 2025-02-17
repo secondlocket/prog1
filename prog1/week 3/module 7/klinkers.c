eeeba064abfb776765c8f3fc4fba9adaa89b8158
UvA Programming Lab <terra@proglab.nl>
2025-02-17 14:39:47 +0100

    Update prog1/week 3/module 7/klinkers.c

--- prog1/week 3/module 7/klinkers.c
+++ prog1/week 3/module 7/klinkers.c
@@ -16,7 +16,7 @@ int main ()
     string woord1 = tolower(get_string("Woord 1 = "));
     string woord2 = tolower(get_string("Woord 2 = "));
     
-    if(woord1 )
+    if(klinkers(woord1) > )
     {
         
     }
