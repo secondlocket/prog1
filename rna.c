d5ce175190b9e901d191b825a0c75b19b877bec1
UvA Programming Lab <terra@proglab.nl>
2025-02-17 19:51:42 +0100

    Update rna.c

--- rna.c
+++ rna.c
@@ -10,7 +10,7 @@ int main()
     string dna = get_string("DNA: ");
     
     //controleren
-    for (i = 0; i < strlen(dna); i++)
+    for (int i = 0; i < strlen(dna); i++)
     {
         char x = toupper(dna[i]);
         
