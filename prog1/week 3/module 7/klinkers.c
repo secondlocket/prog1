c7bbc36397b896a0ce24fdf249a41940e777e9ce
UvA Programming Lab <terra@proglab.nl>
2025-02-17 12:45:40 +0100

    Update prog1/week 3/module 7/klinkers.c

--- prog1/week 3/module 7/klinkers.c
+++ prog1/week 3/module 7/klinkers.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 #include <cs50.h>
+#include <string.h>
 
 int main ()
 {
-    string woord1 = get_string("Woord 1 = ");
-    string woord2 = get_string("Woord 2 = ");
+    string woord1 = tolower(get_string("Woord 1 = "));
+    string woord2 = tolower(get_string("Woord 2 = ");
     
     return 0;
 }
