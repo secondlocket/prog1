e10de02b635e03148156043c7e13d975f8c45851
UvA Programming Lab <terra@proglab.nl>
2025-02-18 11:37:34 +0100

    Update prog1/week 3/module 8/formule.c

--- prog1/week 3/module 8/formule.c
+++ prog1/week 3/module 8/formule.c
@@ -10,7 +10,11 @@ int main()
     
     for (int i = 0; i < strlen(f); i++)
     {
-        
+        if(strchr("AGCT", x) == NULL)
+        {
+            printf("Invalid DNA\n");
+            return 1;
+        }
     }
     
     
