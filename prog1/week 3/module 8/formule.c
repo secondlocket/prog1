4eab035ab230f2c727825e9de14e5c2364a9c803
UvA Programming Lab <terra@proglab.nl>
2025-02-18 11:56:37 +0100

    Update prog1/week 3/module 8/formule.c

--- prog1/week 3/module 8/formule.c
+++ prog1/week 3/module 8/formule.c
@@ -30,14 +30,14 @@ int main()
             }
         }
     
-    if (haakje1 > haakje2)
-    {
-        printf("Er worden te weinig haakjes gesloten\n");
-    }
-    else
-    {
-        printf("Er zijn geen fouten\n");
-    }
+        if (haakje1 > haakje2)
+        {
+            printf("Er worden te weinig haakjes gesloten\n");
+        }
+        else
+        {
+            printf("Er zijn geen fouten\n");
+        }
     }
     
     return 0;
