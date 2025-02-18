64b91bea09f942d6a1341c8fb7ed2a61224d2524
UvA Programming Lab <terra@proglab.nl>
2025-02-18 13:43:48 +0100

    Update cypher.c

--- cypher.c
+++ cypher.c
@@ -13,14 +13,14 @@ int main()
         s[i] = 'a';
         
         for (int i = 0; i < strlen(dna); i++)
-    {
-        char x = toupper(dna[i]);
-        
-        if(strchr("AGCT", x) == NULL)
         {
-            printf("Invalid DNA\n");
-            return 1;
-        }
+            char x = toupper(dna[i]);
+        
+            if(strchr("AGCT", x) == NULL)
+            {
+                printf("Invalid DNA\n");
+                return 1;
+            }
     }
         
         
