94856886b47ad6b620c543d143594b47c9685d76
UvA Programming Lab <terra@proglab.nl>
2025-02-18 14:34:50 +0100

    Update prog1/week 3/module 8/morse_more.c

--- prog1/week 3/module 8/morse_more.c
+++ prog1/week 3/module 8/morse_more.c
@@ -38,6 +38,18 @@ char naar_tekst(string c)
     {
         return 'a';
     }
+    else if (c = ".-")
+    {
+        return 'a';
+    }
+    else if (c = ".-")
+    {
+        return 'a';
+    }
+    else if (c = ".-")
+    {
+        return 'a';
+    }
 }
 
 int main()
