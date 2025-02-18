e609723f28a6dc906134efc7f65e56d4b7bac9c6
UvA Programming Lab <terra@proglab.nl>
2025-02-18 11:47:39 +0100

    Update prog1/week 3/module 8/formule.c

--- prog1/week 3/module 8/formule.c
+++ prog1/week 3/module 8/formule.c
@@ -8,7 +8,7 @@ int main()
     int haakje2;
     
     string string = get_string("Geef een formule: ");
-    char f = string[a]
+    char f = string[int a]
     
     for (int i = 0; i < strlen(f); i++)
     {
