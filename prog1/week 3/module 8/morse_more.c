555bc9492198f126d5827985028e0a910944d909
UvA Programming Lab <terra@proglab.nl>
2025-02-19 15:00:31 +0100

    Update prog1/week 3/module 8/morse_more.c

--- prog1/week 3/module 8/morse_more.c
+++ prog1/week 3/module 8/morse_more.c
@@ -45,12 +45,12 @@ int main()
         if (rauw[i] == ',')
         {
             //karakters afscheiden en printen 1 voor 1 (als het 3x ',' is)
-            else if (strncmp(&rauw[i], ",,,,,,", 6) == 0)
+            if (strncmp(&rauw[i], ",,,,,,", 6) == 0)
             {
                 printf(" ");
                 i += 6;
             }
-            if (strncmp(&rauw[i], ",,,", 3) == 0)
+            else if (strncmp(&rauw[i], ",,,", 3) == 0)
             {
                if (index > 0)
                 {
@@ -61,7 +61,6 @@ int main()
                 i += 2;
             }
             //echte spatie (als het 7x ',' is)
-            
         }
         else
         {
