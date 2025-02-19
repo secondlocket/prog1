3ae63ba4116e0f228ec8868fe4847a78f9d29c96
UvA Programming Lab <terra@proglab.nl>
2025-02-19 14:27:19 +0100

    Update prog1/week 3/module 8/morse_more.c

--- prog1/week 3/module 8/morse_more.c
+++ prog1/week 3/module 8/morse_more.c
@@ -4,13 +4,13 @@
 
 //naar_morse functie (van dit ".", dah "-" en spaties)
 char naar_morse(char *morse) {
-    if (strcmp(morse, ".- ") == 0) return 'a';
-    if (strcmp(morse, "-... ") == 0) return 'b';
-    if (strcmp(morse, "-.-. ") == 0) return 'c';
-    if (strcmp(morse, "-.. ") == 0) return 'd';
-    if (strcmp(morse, ". ") == 0) return 'e';
-    if (strcmp(morse, "..-. ") == 0) return 'f';
-    if (strcmp(morse, "--. ") == 0) return 'g';
+    if (strcmp(morse, ".-") == 0) return 'a';
+    if (strcmp(morse, "-...") == 0) return 'b';
+    if (strcmp(morse, "-.-.") == 0) return 'c';
+    if (strcmp(morse, "-..") == 0) return 'd';
+    if (strcmp(morse, ".") == 0) return 'e';
+    if (strcmp(morse, "..-.") == 0) return 'f';
+    if (strcmp(morse, "--.") == 0) return 'g';
     if (strcmp(morse, ".... ") == 0) return 'h';
     if (strcmp(morse, ".. ") == 0) return 'i';
     if (strcmp(morse, ".--- ") == 0) return 'j';
