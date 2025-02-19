449dcc5b08daf9e3a1c472be38c52e38a5bdd927
UvA Programming Lab <terra@proglab.nl>
2025-02-19 13:26:29 +0100

    Update prog1/week 3/module 8/morse_more.c

--- prog1/week 3/module 8/morse_more.c
+++ prog1/week 3/module 8/morse_more.c
@@ -32,7 +32,7 @@ int main()
                 if (nummer > 0)
                 {
                     morse[nummer] = '\0';
-                    printf("%c")
+                    printf("%c", )
                     nummer = 0;
                 }
             }
