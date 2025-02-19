497b288407719dd3661d7e2af85dc8e5d6de2b3e
UvA Programming Lab <terra@proglab.nl>
2025-02-19 12:21:50 +0100

    Update prog1/week 3/module 8/morse_more.c

--- prog1/week 3/module 8/morse_more.c
+++ prog1/week 3/module 8/morse_more.c
@@ -16,7 +16,7 @@ int main()
             if (strncmp(&rauw[i + 1], ",,,,,,", rauw[i + 6]) == 0)
             {
                 printf(" ");
-                
+                i += 6;
             }
         }
         
