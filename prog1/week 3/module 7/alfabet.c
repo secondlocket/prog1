4bb25dd5dc7a95fc2ca39b34510a832102960d65
UvA Programming Lab <terra@proglab.nl>
2025-02-20 13:21:12 +0100

    Update prog1/week 3/module 7/alfabet.c

--- prog1/week 3/module 7/alfabet.c
+++ prog1/week 3/module 7/alfabet.c
@@ -8,7 +8,10 @@ int main ()
     string woord1 = get_string("Woord 1: ");
     string woord2 = get_string("Woord 2: ");
     
-    if (strlen(woord1))
+    if (strlen(woord1) > strlen(woord2))
+    {
+        
+    }
     
     for (int i = 0; i < strlen(woord1); i++)
     {
