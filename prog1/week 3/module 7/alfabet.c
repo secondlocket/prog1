3600de9a76f7e45a87c76c11f6fee803c8dde092
UvA Programming Lab <terra@proglab.nl>
2025-02-20 12:39:39 +0100

    Update prog1/week 3/module 7/alfabet.c

--- prog1/week 3/module 7/alfabet.c
+++ prog1/week 3/module 7/alfabet.c
@@ -10,6 +10,8 @@ int main ()
     for (i = 0; i < strlen(woord1); i++)
     {
         woord1[i] = tolower(woord1[i]);
+        
+        if (woord[i])
     }
     
     for (i = 0; i < strlen(woord2); i++)
