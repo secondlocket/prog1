b628db5d832a5cf7de91d212cf69a3ae2672abea
UvA Programming Lab <terra@proglab.nl>
2025-02-20 13:36:39 +0100

    Update prog1/week 3/module 7/alfabet.c

--- prog1/week 3/module 7/alfabet.c
+++ prog1/week 3/module 7/alfabet.c
@@ -47,7 +47,7 @@ int main ()
             {
                 if (strlen(woord2) == strlen(woord1))
                 {
-                    
+                    printf
                 }
                 else if (n == strlen(woord2))
                 {
