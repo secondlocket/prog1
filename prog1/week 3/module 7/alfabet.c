ca3040ba230fd7cfc32950003c937ed349791acb
UvA Programming Lab <terra@proglab.nl>
2025-02-20 13:54:30 +0100

    Update prog1/week 3/module 7/alfabet.c

--- prog1/week 3/module 7/alfabet.c
+++ prog1/week 3/module 7/alfabet.c
@@ -10,7 +10,7 @@ int main ()
     string woord1 = get_string("Woord 1: ");
     string woord2 = get_string("Woord 2: ");
     
-    
+    char 
     
     //controleren welke langer is
     if (strlen(woord1) > strlen(woord2))
