23a7f6955a1a2a027b16f27636a57947613f6abd
UvA Programming Lab <terra@proglab.nl>
2025-02-25 15:06:14 +0100

    Update prog1/week 3/module 7/wachtwoord.c

--- prog1/week 3/module 7/wachtwoord.c
+++ prog1/week 3/module 7/wachtwoord.c
@@ -10,7 +10,7 @@ int main ()
     {
         string invoer = get_string("Wachtwoord? ");
     }
-    while (sterk(invoer) == 1);
+    while (lengte == 1);
     
     
     for (i = 0; i < strlen(invoer); i++)
