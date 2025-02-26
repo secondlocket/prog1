34583cb59fc38e0843b0442ac9fd77715ba443d5
UvA Programming Lab <terra@proglab.nl>
2025-02-26 14:25:16 +0100

    Update prog1/week 3/module 8/calendar.c

--- prog1/week 3/module 8/calendar.c
+++ prog1/week 3/module 8/calendar.c
@@ -8,7 +8,7 @@ int main ()
     
     //hoeveel jaren om te tellen
     int y = year - 1800
-    (y % 4) + 365 * 
+    (y % 4) + 365 * (y % 4 * 3)
     
     //display calendar
         //display header
