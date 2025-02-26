54b5a8f359c42596b3df05fbdbd284529e7efc68
UvA Programming Lab <terra@proglab.nl>
2025-02-26 14:19:10 +0100

    Update prog1/week 3/module 8/calendar.c

--- prog1/week 3/module 8/calendar.c
+++ prog1/week 3/module 8/calendar.c
@@ -10,7 +10,7 @@ int main ()
         //display header
         //display grid
             //first day of month
-            (from_1800 + 
+            (from_1800 + start_1800) % 7
             //get numbers of month
             //print spaces as padding so day 1 is correctly aligned
             //print the number grid
