32e51aadfc865573e2df2ce0181b5bff51aeae4c
UvA Programming Lab <terra@proglab.nl>
2025-02-26 15:11:11 +0100

    Update prog1/week 3/module 8/calendar.c

--- prog1/week 3/module 8/calendar.c
+++ prog1/week 3/module 8/calendar.c
@@ -5,25 +5,27 @@ void display_calendar();
 
 int main ()
 {
-    int dag = 3;
     int year = get_int("Year: ");
     string month = get_string("Month: ");
-
-    //calendar weergeven
-    //header weergeven
-    printf("          %s %i          \n", month, year);
+    
+    //hoeveel jaren om te tellen
+    int y = year - 1800;
+    //hoeveel dagen tussen invoer en 1800
+    int dagen = (y % 4) + 365 * (y % 4 * 3);
+    //display calendar
+    //display header
+    printf("          %s %i          ", month, year);
     for (int i = 0; i < 27; i++)
     {
         printf("-");
     }
-    printf("\n");
 
-    //display grid
-        //first day of month
-        (dagen + start_1800) % 7
-        //get numbers of month
-        //print spaces as padding so day 1 is correctly aligned
-        //print the number grid
+        //display grid
+            //first day of month
+            (from_1800 + start_1800) % 7
+            //get numbers of month
+            //print spaces as padding so day 1 is correctly aligned
+            //print the number grid
     
     return 0;
 }
