1f3cde3f0d56ada6facc4718976383752bf6c079
UvA Programming Lab <terra@proglab.nl>
2025-03-10 17:46:23 +0100

    Update prog1/week 3/module 8/calendar.c

--- prog1/week 3/module 8/calendar.c
+++ prog1/week 3/module 8/calendar.c
@@ -89,7 +89,8 @@ void display_calendar(int year, int month)
 
 void display_header(int year, int month);
 {
-    printf("Year = %i")
+    printf("Year = %i", year);
+    printf
     printf("          %s %i          ", month, year);
     for (int i = 0; i < 27; i++)
     {
