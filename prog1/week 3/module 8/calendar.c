db932001c620a4fbef3eead2b2d3d1b9bbb4eef0
UvA Programming Lab <terra@proglab.nl>
2025-03-10 14:48:52 +0100

    Update prog1/week 3/module 8/calendar.c

--- prog1/week 3/module 8/calendar.c
+++ prog1/week 3/module 8/calendar.c
@@ -2,7 +2,11 @@
 #include <cs50.h>
 #include <string.h>
 
-
+//int year
+//string month
+//int y = jaren verschil
+//int d = dagen verschil
+//int dag(string maand) geeft welke begindag terug
 
 //geeft maand nummer terug
 int maand_nummer(string month)
