40f1c60fd05b7191adbbc80509c3d47d74d36ae3
UvA Programming Lab <terra@proglab.nl>
2025-03-15 18:42:23 +0100

    Update prog1/week 3/module 8/calendar.c

--- prog1/week 3/module 8/calendar.c
+++ prog1/week 3/module 8/calendar.c
@@ -11,6 +11,7 @@ void display_header(int year, int month);
 void display_grid(int year, int month);
 int maand_nummer(string month);
 int dag(int year, int month);
+int sinds1800(int year, int month)
 int dagen_pm(int year, int month);
 int leap(int year);
 
