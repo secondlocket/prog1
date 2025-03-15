ae3406d63ceb60d2cda6cc14a2744404d9a9cff4
UvA Programming Lab <terra@proglab.nl>
2025-03-15 18:41:55 +0100

    Update prog1/week 3/module 8/calendar.c

--- prog1/week 3/module 8/calendar.c
+++ prog1/week 3/module 8/calendar.c
@@ -101,6 +101,9 @@ int dag(int year, int month)
     return (dagen + 3) % 7;
 }
 
+
+
+
 int dagen_pm(int year, int month)
 {
     if (month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12)
