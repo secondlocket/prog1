d97ac470a5c2156b69dae9ba76e9d4a8e25fdf47
UvA Programming Lab <terra@proglab.nl>
2025-03-16 22:22:24 +0100

    Update prog1/week 5/module 10/find.c

--- prog1/week 5/module 10/find.c
+++ prog1/week 5/module 10/find.c
@@ -92,7 +92,11 @@ void generate_sequential_numbers(int arr[], int n)
 {
     // TODO: implement a sequential number generator
     
-    arr[i] = (int) (drand48() * LIMIT);
+    for (int i = 0; i < n; i++)
+    {
+        // random number between 0 and LIMIT
+        arr[i] = (int) (drand48() * LIMIT);
+    }
     
     
     return;
