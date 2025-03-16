8bb12e18c8d48187f4cfc4338b708f6a40cdaedd
UvA Programming Lab <terra@proglab.nl>
2025-03-16 23:00:56 +0100

    Update prog1/week 5/module 10/find.c

--- prog1/week 5/module 10/find.c
+++ prog1/week 5/module 10/find.c
@@ -103,6 +103,8 @@ bool search(int value, int values[], int n)
 {
     // TODO: implement the searching algorithm
     
+    
+    
     for (int i = 0; i < n; i++)
     {
         if (values[i] == value)
