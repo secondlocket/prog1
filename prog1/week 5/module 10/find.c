fbd1eb56093254b0214269ea5293b32ebd4065fb
UvA Programming Lab <terra@proglab.nl>
2025-03-17 00:00:56 +0100

    Update prog1/week 5/module 10/find.c

--- prog1/week 5/module 10/find.c
+++ prog1/week 5/module 10/find.c
@@ -133,7 +133,8 @@ void sort(int values[], int n)
 {
     // TODO: implement the sorting algorithm
     
-    for (int i = 0; i < n - 1; i++)
+    int max = values[0];
+    for (int i = 1; i < n; i++)
     {
         
     }
