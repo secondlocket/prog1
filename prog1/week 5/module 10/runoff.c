d47db1e22b25ba85f47097aaedcb9faa8ec8f91c
UvA Programming Lab <terra@proglab.nl>
2025-03-17 19:38:02 +0100

    Update prog1/week 5/module 10/runoff.c

--- prog1/week 5/module 10/runoff.c
+++ prog1/week 5/module 10/runoff.c
@@ -127,6 +127,8 @@ int main(int argc, string argv[])
 // Record preference if vote is valid
 bool vote(int voter, int rank, string name)
 {
+    int j; 
+    
     for (int i = 0; i < candidate_count; i++)
     {
         if (strcmp(name, candidates[i].name) == 0)
