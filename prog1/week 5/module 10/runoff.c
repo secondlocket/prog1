6742001bcd23baa5fa65257474d80b7de74c7a26
UvA Programming Lab <terra@proglab.nl>
2025-03-17 18:41:13 +0100

    Update prog1/week 5/module 10/runoff.c

--- prog1/week 5/module 10/runoff.c
+++ prog1/week 5/module 10/runoff.c
@@ -127,6 +127,10 @@ int main(int argc, string argv[])
 bool vote(int voter, int rank, string name)
 {
     for (int i = 0; i < candidate_count; i++)
+    {
+        
+    }
+    
     return false;
 }
 
