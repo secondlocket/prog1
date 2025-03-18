fb40fd735590c3d8105752b7a28b61fecf01cbd7
UvA Programming Lab <terra@proglab.nl>
2025-03-18 15:01:08 +0100

    Update prog1/week 5/module 10/runoff.c

--- prog1/week 5/module 10/runoff.c
+++ prog1/week 5/module 10/runoff.c
@@ -177,6 +177,7 @@ int find_min(void)
     // TODO
     for (int i = 0; i < candidate_count; i++)
     {
+        int max = 0
         if (candidates[i].eliminated = false)
         {
             for (int j = 0; i < candidate_count; j++)
