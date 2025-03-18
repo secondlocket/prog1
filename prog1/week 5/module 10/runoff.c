5d710caf8abccae78fcb95250114f1430a4b63c3
UvA Programming Lab <terra@proglab.nl>
2025-03-18 15:08:00 +0100

    Update prog1/week 5/module 10/runoff.c

--- prog1/week 5/module 10/runoff.c
+++ prog1/week 5/module 10/runoff.c
@@ -175,7 +175,7 @@ bool print_winner(void)
 int find_min(void)
 {
     // TODO
-    int min = ;
+    int min = INT_MAX;
     for (int i = 0; i < candidate_count; i++)
     {
         if (candidates[i].eliminated = false && candidates[i].votes < min)
