d9a02f2545fa278db71f5d19eb6766b9ff9b6fef
UvA Programming Lab <terra@proglab.nl>
2025-03-19 15:21:39 +0100

    Update prog1/week 5/module 10/runoff.c

--- prog1/week 5/module 10/runoff.c
+++ prog1/week 5/module 10/runoff.c
@@ -162,7 +162,10 @@ void tabulate(void)
 bool print_winner(void)
 {
     // TODO
-    //check
+    for (int i = 0; i < candidate_count; i++)
+    {
+        
+    }
     if (candidate[i].votes > voter_count / 2)
     {
         printf("%s", candidate[i].name);
