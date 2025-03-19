f902cea2c2a91429d8e67c2c6ff38de72e5ba6cf
UvA Programming Lab <terra@proglab.nl>
2025-03-19 16:01:55 +0100

    Update prog1/week 5/module 11/tiles.c

--- prog1/week 5/module 11/tiles.c
+++ prog1/week 5/module 11/tiles.c
@@ -248,7 +248,7 @@ bool move(int tile)
         }
     }
     
-    if (tile_i == empty_i && abs(tile_j - empty_j == 1)
+    if (tile_i == empty_i && abs(tile_j - empty_j) == 1)
     
     return false;
 }
