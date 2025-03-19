8209a7a2f8b70386b125953154ba2cc3fece2346
UvA Programming Lab <terra@proglab.nl>
2025-03-19 15:23:01 +0100

    Update prog1/week 5/module 10/runoff.c

--- prog1/week 5/module 10/runoff.c
+++ prog1/week 5/module 10/runoff.c
@@ -147,6 +147,10 @@ void tabulate(void)
     // TODO
     for (int i = 0; i < voter_count; i++)
     {
+        for (int j = 0; j < candidate_count; j++)
+        {
+            
+        }
         if (candidates[i].eliminated = false && preferences[i][0])
         {
             candidates[i].votes++;
