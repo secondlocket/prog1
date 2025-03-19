09283560a83f0583ea977bb14975824e80848f85
UvA Programming Lab <terra@proglab.nl>
2025-03-19 15:48:22 +0100

    Update prog1/week 5/module 11/tiles.c

--- prog1/week 5/module 11/tiles.c
+++ prog1/week 5/module 11/tiles.c
@@ -174,7 +174,10 @@ void init_board(void)
     
     for (int i = 0; i < d; i++)
     {
-        for (int j = 0; )
+        for (int j = 0; j < d; j++)
+        {
+            board[i][j]
+        }
     }
     
 }
