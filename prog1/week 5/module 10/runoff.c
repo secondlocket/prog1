f08e0c45e13f901c7d5d145b5eb970b755b65494
UvA Programming Lab <terra@proglab.nl>
2025-03-19 15:12:31 +0100

    Update prog1/week 5/module 10/runoff.c

--- prog1/week 5/module 10/runoff.c
+++ prog1/week 5/module 10/runoff.c
@@ -175,7 +175,7 @@ bool print_winner(void)
 // Return the minimum number of votes any remaining candidate has
 int find_min(void)
 {
-    // TODO
+    // TODO done
     int min = INT_MAX;
     for (int i = 0; i < candidate_count; i++)
     {
