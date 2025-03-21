8ee66cf2d691eff8f3bdb39c367a6cf1a4aadf33
UvA Programming Lab <terra@proglab.nl>
2025-03-21 02:18:21 +0100

    Update prog1/extra/module 11/snowfall.c

--- prog1/extra/module 11/snowfall.c
+++ prog1/extra/module 11/snowfall.c
@@ -104,5 +104,5 @@ void pauze (int t)
     struct timespec ts;
     ts.tv_sec = t / 1000; //timespec.tvseconden
     ts.tv_nsec = (t % 1000) * 1000000; //timespec.tv_nanoseconden
-    nanosleep(&ts, NULL); //
+    nanosleep(&ts, NULL); //(spe)
 }
