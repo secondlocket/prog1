7e99e074a76786b375bc6f1bae1f9baa2a787026
UvA Programming Lab <terra@proglab.nl>
2025-03-24 17:51:52 +0100

    Update prog1/oefening/makelaar.c

--- prog1/oefening/makelaar.c
+++ prog1/oefening/makelaar.c
@@ -6,12 +6,13 @@ int main()
 {
     string invoer = get_string("");
     
+    int wijk = 0;
     int n = strlen(invoer);
     for (int i = 0; i < n; i++)
     {
         if (strcmp(invoer, "wijk 1"))
         {
-            int wijk = 1;
+            wijk = 1;
         }
     }
 }
