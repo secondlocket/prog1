be37175d3c6f3b2026d1423f0fdb9f009b5a7d40
secondlocket <benjaminchan031@gmail.com>
2025-03-25 19:26:41 +0100

    Update collatz.c

--- prog1/oefening/collatz.c
+++ prog1/oefening/collatz.c
@@ -1,1 +1,27 @@
+#include <stdio.h>
+#include <cs50.h>
 
+int main()
+{
+    int invoer = get_int("Collatz beginnend bij ");
+    int getal;
+    int stap = 1;
+
+    while (getal != 1)
+    {
+          printf("\n Stap %i: %i", stap, invoer)
+          
+          if (invoer % 2 == 0)
+          {
+              getal = invoer / 2; 
+              stap++;
+          }
+          else
+          {
+              getal = invoer * 3 + 1;
+              stap++;
+          }
+    }
+
+    return 0;
+}
