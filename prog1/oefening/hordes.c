0fc1b316ba2fa58b640d4703da50b829eba0f375
UvA Programming Lab <terra@proglab.nl>
2025-03-26 18:52:54 +0100

    Update prog1/oefening/hordes.c

--- prog1/oefening/hordes.c
+++ prog1/oefening/hordes.c
@@ -18,7 +18,8 @@ int tel_hordes(int afstanden[], int n, int baanlengte)
         {
             ronde++;
         }
-        if 
+        
+        if (afstanden) 
     }
     
     totaal_afstand += (baanlengte - laatste_afstand) * ronde + (laatste_afstand * ronde);
