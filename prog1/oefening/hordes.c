5f1f0928f9355fbed240cc03ac561ef2f4bc2a0d
UvA Programming Lab <terra@proglab.nl>
2025-03-26 18:34:51 +0100

    Update prog1/oefening/hordes.c

---
+++ prog1/oefening/hordes.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <cs50.h>
+
+int tel_hordes(int afstanden[], int n, int baanlengte)
+{
+    //TODO
+    return 0;
+}
+
+int main(void)
+{
+    int afstanden1[]
+    int n1 =
+}
