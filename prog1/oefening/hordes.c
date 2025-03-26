5fcdc892b6e670011e2f24cbd385668f8d852e78
UvA Programming Lab <terra@proglab.nl>
2025-03-26 19:42:15 +0100

    Update prog1/oefening/hordes.c

--- prog1/oefening/hordes.c
+++ prog1/oefening/hordes.c
@@ -10,7 +10,7 @@ int tel_hordes(int afstanden[], int n, int baanlengte)
         int huidige = afstanden[i];
         int vorige = afstanden[i - 1];
         
-        if (huidige >=)
+        if (huidige >= vorige)
         {
             hordes += (huidige / 30) - (vorige / 30);
         }
