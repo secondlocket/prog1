eafb203f9f0b9950429c13c189cbec4f60dd5003
UvA Programming Lab <terra@proglab.nl>
2025-03-26 18:37:58 +0100

    Update prog1/oefening/hordes.c

--- prog1/oefening/hordes.c
+++ prog1/oefening/hordes.c
@@ -15,7 +15,7 @@ int main(void)
     printf("%i\n", tel_hordes(afstanden1, n1, baanlengte1));
     
     int afstanden2[] = {0, 40, 75, 5, 35, 70, 100};
-    int n2 = ;
+    int n2 = 7;
     int baanlengte2 = ;
     printf("%i\n", tel_hordes(afstanden2, n2, baanlengte2));
     
