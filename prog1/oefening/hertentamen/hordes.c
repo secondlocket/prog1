106f9471b1065bc816616933db08ba54a256f71e
UvA Programming Lab <terra@proglab.nl>
2025-03-31 18:18:10 +0200

    Update prog1/oefening/hertentamen/hordes.c

--- prog1/oefening/hertentamen/hordes.c
+++ prog1/oefening/hertentamen/hordes.c
@@ -6,7 +6,7 @@ int tel_hordes(int afstanden[], int n, int baanlengte)
 {
     for (int i = 0; i < n; i++)
     {
-        if (afstanden[i] > afstanden[])
+        if (afstanden[i] > afstanden[i + 1])
     }
     
     return hordes;
