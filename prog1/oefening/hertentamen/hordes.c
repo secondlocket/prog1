2aa0346f6f91ba0380234807629170b9da2c9346
UvA Programming Lab <terra@proglab.nl>
2025-03-31 18:21:11 +0200

    Update prog1/oefening/hertentamen/hordes.c

--- prog1/oefening/hertentamen/hordes.c
+++ prog1/oefening/hertentamen/hordes.c
@@ -7,7 +7,7 @@ int tel_hordes(int afstanden[], int n, int baanlengte)
     int ronde = 0;
     int aantal_hordes = round(baanlengte / 30);
     int totaal_afstand = 0;
-    int laaste_afstand = 
+    int laaste_afstand = baanlengte % 30;
     
     for (int i = 0; i < n; i++)
     {
